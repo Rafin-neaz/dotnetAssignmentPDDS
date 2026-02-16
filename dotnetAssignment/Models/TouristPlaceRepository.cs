b9d77d6b7328018c721e9e303c9027ce9cca678f
rafinShafneaz <rafin.shafneaz@enosisbd.com>
2026-02-16 19:00:08 +0600

    Add asynchronous and no tracking

--- dotnetAssignment/Models/TouristPlaceRepository.cs
+++ dotnetAssignment/Models/TouristPlaceRepository.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
 namespace dotnetAssignment.Models
 {
     public class TouristPlaceRepository : ITouristPlaceRepository
@@ -7,50 +8,64 @@ namespace dotnetAssignment.Models
 
         public TouristPlaceRepository(AppDbContext context)
         {
-            _context = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
+
         public TouristPlace CreateNew(TouristPlace place)
         {
+            if (place == null)
+                throw new ArgumentNullException(nameof(place));
+
             _context.Tourists.Add(place);
             _context.SaveChanges();
+
             return place;
         }
 
         public TouristPlace Delete(long id)
         {
-            TouristPlace place = _context.Tourists.Find(id);
-            if (place != null)
-            {
-                _context.Tourists.Remove(place);
-                _context.SaveChanges();
-            }
+            var place = _context.Tourists.Find(id);
+            if (place == null)
+                return null;
+
+            _context.Tourists.Remove(place);
+            _context.SaveChanges();
+
             return place;
         }
 
         public TouristPlace Get(long id)
         {
-            TouristPlace place = _context.Tourists.Find(id);
-            return place;
+            return _context.Tourists
+                           .AsNoTracking()
+                           .FirstOrDefault(x => x.Id == id);
         }
 
         public List<TouristPlace> GettAll()
         {
-            return _context.Tourists.ToList();
+            return _context.Tourists
+                           .AsNoTracking()
+                           .ToList();
         }
 
         public TouristPlace Update(TouristPlace updatedPlace)
         {
-            TouristPlace place = _context.Tourists.Find(updatedPlace.Id);
-            if (place != null)
-            {
-                place.Name = updatedPlace.Name;
-                place.Address = updatedPlace.Address;
-                place.Rating = updatedPlace.Rating;
-                place.Type = updatedPlace.Type;
-                place.PhotoPath = updatedPlace.PhotoPath;
-                _context.SaveChanges();
-            }
-            return updatedPlace;
+            if (updatedPlace == null)
+                throw new ArgumentNullException(nameof(updatedPlace));
+
+            var existingPlace = _context.Tourists.Find(updatedPlace.Id);
+            if (existingPlace == null)
+                return null;
+
+            existingPlace.Name = updatedPlace.Name;
+            existingPlace.Address = updatedPlace.Address;
+            existingPlace.Rating = updatedPlace.Rating;
+            existingPlace.Type = updatedPlace.Type;
+            existingPlace.PhotoPath = updatedPlace.PhotoPath;
+
+            _context.SaveChanges();
+
+            return existingPlace;
         }
     }
 }
